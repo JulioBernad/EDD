17b4807a6c525532c256007833e600c9b310919d
JulioBernad <juliobernadcastejon@hotmail.com>
2016-03-14 19:00:15 +0100

    Julio Bernad
    Signed-off-by: JulioBernad <juliobernadcastejon@hotmail.com>

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,6 +2,7 @@ package fp;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
@@ -11,59 +12,173 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
 
 	/*
-     * devuelve una lista con los n n√∫meros de la serie de fibonacci.
+     * devuelve una lista con los n n˙meros de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		ArrayList<Integer> array;
+		array = new ArrayList<Integer>();
+		Integer integer = new Integer(n);
+		Integer e = null;
+		array.add(e);
+		return array;
 	}
 
-	/*
-	 * Escribir todos los n√∫meros del number al 0 de step en step.
-	 */
-	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
+	
+	public static int[] saltitos(int n, int s){
+		if(s==0)
+			return new int[0];
+		int length=(n-1)/s;
+		int saltos[]=new int[length];
+		int i=0;
+		while(n>s){
+			n-=s;
+			saltos[i]=n;
+			i++;
 		}
-
-	/*
-	 * M√≥dulo al que se le pasa un n√∫mero entero del 0 al 20 y devuelve los
-	 * divisores que tiene.
-	 */
-	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		return saltos;
+	}
+	
+	public static int[] divisores(int n){
+		int length=0;
+		if(n>0){
+			for(int i=1; i<=n; i++)
+				if(n%i==0)
+					length++;
+		}else return null;
+		int div[] = new int[length];
+		try{	
+			for (int i = n, j = 0; i > 0; i--) {
+				if(n % i == 0){
+					div[j] = i;
+					j++;
+				}
+			}
+			return div;
+		}catch(IndexOutOfBoundsException e){
+			return null;
+		}
+		
 	}
 
-	/*
-	 * Toma como par√°metros una cadena de caracteres y devuelve cierto si la cadena resulta ser un pal√≠ndromo
-	 */
-	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+	
+	public static boolean palindromo(String frase){
+		
+		if(frase==null)
+			return false;
+		String cadena="";
+		String tildes = "·‡‰ÈËÎÌÏÔÛÚˆ˙˘uÒ¡¿ƒ…»ÀÕÃœ”“÷⁄Ÿ‹—Á«";
+		String ascii = "aaaeeeiiiooouuunAAAEEEIIIOOOUUUNcC";
+		String reverseFrase="";
+		frase=frase.toLowerCase();
+		
+		for(int i=0;i<frase.length();i++)
+			if(Character.isLetter(frase.charAt(i)))
+				if(tildes.indexOf(frase.charAt(i))!=-1)
+					cadena+=ascii.charAt(tildes.indexOf(frase.charAt(i)));
+				else
+					cadena+=frase.charAt(i);
+		for(int i=cadena.length()-1;i>=0;i--)
+			reverseFrase+=cadena.charAt(i);
+		if(cadena.equals(reverseFrase))
+			return true;
+		else return false;
 	}
 
-	/*
-	 * Pedir un n√∫mero de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56
-	 * mostrar: cincuenta y seis
-	 */
-	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+	  public static String hablame(int numero){
+		  
+	  String[] decenas = {"Cero","Diez","Veinte","Treinta","Cuarenta","Cincuenta",
+			  "Sesenta","Setenta","Ochenta","Noventa"};
+
+	  String[] unidades = {"","uno","dos","tres","cuatro","cinco","seis","siete",
+			  "ocho","nueve","Diez","Once","Doce","Trece","Catorce","Quince","Dieciseis",
+			  "Diecisiete","Dieciocho","Diecinueve"};
+  
+	  String numescrito="";
+	  
+	  if(numero==0){
+		  numescrito="";
+		  numescrito=decenas[0];
+		  return numescrito;
+	  }
+	  else if(numero<20){
+		  numescrito="";
+		  numescrito=unidades[numero];
+		  return numescrito;
+	  }
+	  else if((numero%10)==0){
+		  numescrito="";
+		  numescrito=decenas[numero/10];
+		  return numescrito;
+	  }
+	  else{
+		  numescrito="";
+		  numescrito=decenas[numero/10]+" y "+unidades[numero%10];
+		  return numescrito;
+	  }
+	 }
+	
+	public static boolean bisiesto(String fecha){
+		int anyo;
+		try {
+			anyo = Integer.parseInt(fecha.substring(6));
+		}catch (NumberFormatException e) {
+			return false;
+		}catch(IndexOutOfBoundsException i){
+			return false;
+		}
+		if((anyo%100)==0 && (anyo%400)!=0)
+			return false;
+		if((anyo%4)==0)
+			return true;
+		else
+			return false;
 	}
 
-	/*
-	 * este metodo devuelve cierto si el a√±o de la fecha es bisiesto fecha
-	 * dd-MM-yyyy
-	 */
-	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
-	}
 
-	/*
-	 * este metodo devuelve cierto si la fecha es v√°lida
-	 */
-	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+	public static boolean fechaValida(String fecha){
+		int dia, mes, anyo;
+		
+		try{
+			//01-01-2001
+			dia=Integer.parseInt(fecha.substring(0, 2));
+			mes=Integer.parseInt(fecha.substring(3,5));
+			anyo=Integer.parseInt(fecha.substring(6));
+		}catch (NumberFormatException e) {
+			return false;
+		}catch(IndexOutOfBoundsException i){
+			return false;
+		}
+		if(anyo>0){
+			switch(mes){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:				
+				if(dia<=31 && dia>0)
+					return true;
+					break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if(dia<=30 && dia>0)
+					return true;
+					break;
+			case 2:
+				if(dia<=28 && dia>0)
+					return true;
+					break;
+			}
+		}		
+		return false;
 	}
 }
+
